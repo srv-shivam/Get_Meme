8db1203bc0942b30ffcdc7e34f099462336bd392
srv-shivam <shivamsrivastava222001@gmail.com>
2021-10-06 00:44:06 +0530

    worked on loading of new memes and to download them.

--- app/src/main/java/com/example/getmeme/MainActivity.kt
+++ app/src/main/java/com/example/getmeme/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.getmeme
 
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.drawable.Drawable
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -16,6 +22,10 @@ import com.bumptech.glide.request.target.Target
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var memeURL: String? = null
+    var myDownloadId: Long? = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,9 +37,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnDownload.setOnClickListener {
+            downloadMeme()
+
+            val broadcastReceiver = object : BroadcastReceiver() {
+                override fun onReceive(context: Context?, intent: Intent?) {
+                    val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+                    if (myDownloadId == id) {
+                        Toast.makeText(this@MainActivity, "Meme downloaded :)", Toast.LENGTH_LONG)
+                            .show()
+                    }
+                }
+            }
+
+            registerReceiver(
+                broadcastReceiver,
+                IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE)
+            )
         }
 
     }
+
+    private fun downloadMeme() {
+
+        val request = DownloadManager.Request(Uri.parse(memeURL))
+            .setDescription("Downloading meme....")
+            .setTitle("Get Meme")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+            .setAllowedOverMetered(true)
+
+        val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        myDownloadId = downloadManager.enqueue(request)
+    }
+
 
     private fun loadMeme() {
 
@@ -42,8 +81,8 @@ class MainActivity : AppCompatActivity() {
         // Request a jsonObject response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             { response ->
-                val memeURL = response.getString("url")
-                Glide.with(this).load(memeURL).listener(object: RequestListener<Drawable> {
+                memeURL = response.getString("url")
+                Glide.with(this).load(memeURL).listener(object : RequestListener<Drawable> {
                     override fun onLoadFailed(
                         e: GlideException?,
                         model: Any?,
@@ -69,11 +108,10 @@ class MainActivity : AppCompatActivity() {
                 }).into(ivMemeImage)
             },
             {
-               Toast.makeText(this, "Something went wrong !!", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Something went wrong !!", Toast.LENGTH_LONG).show()
             })
 
         queue.add(jsonObjectRequest)
-
     }
 
 
